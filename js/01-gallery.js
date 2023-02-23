bf81245ed9eb9689ec40849593862faed63431d3
Jacek Pasierb <jack_byk@o2.pl>
2023-02-23 16:31:11 +0100

    Created structure gallery for task i added files with gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+for (const og of galleryItems) {
+   
+    let div = document.createElement("div");
+    div.classList.add("gallery__item");
+    gallery.append(div);
+
+    let a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.href = og.original;
+    div.append(a);
+
+    let img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = og.preview;
+    img.dataset.source = og.original;
+    img.alt = og.description;
+    a.append(img);
+}
+
+
