401fd8aff8b51ddd4e0f9e05fcbbbe1a1e3ba282
Jacek Pasierb <jack_byk@o2.pl>
2023-02-23 19:36:16 +0100

    Added basicLightbox and created events

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
 
 for (const og of galleryItems) {
-   
     let div = document.createElement("div");
     div.classList.add("gallery__item");
     gallery.append(div);
@@ -22,6 +19,18 @@ for (const og of galleryItems) {
     img.dataset.source = og.original;
     img.alt = og.description;
     a.append(img);
+
+    gallery.querySelector("div.gallery__item").onclick = () => {
+        preventDefault();
+      basicLightbox
+        .create(
+          `
+		<img width="1400" height="900" src="${og.original}">
+	`
+        )
+        .show();
+    };
 }
+
 
 
