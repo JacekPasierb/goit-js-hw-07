99adba7b1fc945ecf541dc905aea39afbfa565c8
Jacek Pasierb <jack_byk@o2.pl>
2023-02-25 00:45:13 +0100

    Done gallery for simpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+for (const objectGallery of galleryItems){
+
+    let a = document.createElement("a");
+    a.classList.add("gallery__item");
+    a.href = objectGallery.original;
+    gallery.append(a);
+
+    let img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = objectGallery.preview;
+    img.alt = objectGallery.description;
+    a.append(img);
+
+}
+
+
+const options = {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+};
+ gallery = new SimpleLightbox(".gallery a",options);
